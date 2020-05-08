179bd220334655ba8dcd7809d42a7dcc6cbec049
skrgiaooo <64412084+skrgiaooo@users.noreply.github.com>
2020-05-08 14:10:59 +0800

    覆盖原来user表

--- MXonline1/settings.py
+++ MXonline1/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.users.apps.UsersConfig',
+    'apps.courses.apps.CoursesConfig',
 ]
 
 MIDDLEWARE = [
@@ -118,7 +120,7 @@ USE_L10N = True
 
 USE_TZ = False
 
-
+AUTH_USER_MODEL = 'users.UserProfile'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
